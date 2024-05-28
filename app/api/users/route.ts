dcf7e8ae34448b3aa5cd1b01ee653212e29c57e3
boysimon10 <simondiouf2710@gmail.com>
2024-05-28 22:34:53 +0000

    get users

---
+++ app/api/users/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+import User from '../../../models/user.model';
+import { connectToDB } from '../../../db/db';
+
+export async function GET(request: NextRequest) {
+    try {
+      await connectToDB();
+  
+      const allUsers = await User.find().select('-password');;
+  
+      return NextResponse.json(allUsers, { status: 200 });
+    } catch (err) {
+      console.error(err);
+      return new NextResponse("Failed to get all users", { status: 500 });
+    }
+  }
